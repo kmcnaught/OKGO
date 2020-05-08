5efa48b0e445d7a43c09dd124bebead99bd50dc8
K McNaught <kirsty.mcnaught@gmail.com>
2020-05-08 15:47:39 +0100

    Copy over management console from EyeMine project
    We'll instantiate one of these instead of the core Optikey one

---
+++ src/JuliusSweetland.OptiKey.EyeMine/UI/Windows/ManagementWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2020 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+using System.Windows;
+using JuliusSweetland.OptiKey.Properties;
+using JuliusSweetland.OptiKey.Services;
+using JuliusSweetland.OptiKey.UI.ViewModels;
+using MahApps.Metro.Controls;
+
+namespace JuliusSweetland.OptiKey.EyeMine.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for ManagementWindow.xaml
+    /// </summary>
+    public partial class ManagementWindow : MetroWindow
+    {
+        public ManagementWindow(
+            IAudioService audioService,
+            IDictionaryService dictionaryService,
+            IWindowManipulationService windowManipulationService)
+        {
+            InitializeComponent();
+
+            //Instantiate ManagementViewModel and set as DataContext of ManagementView
+            var managementViewModel = new ManagementViewModel(audioService, dictionaryService, windowManipulationService);
+            this.ManagementView.DataContext = managementViewModel;
+        }
+    }
+}
