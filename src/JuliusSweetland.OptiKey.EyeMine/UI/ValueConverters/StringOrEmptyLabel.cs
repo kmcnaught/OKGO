588d76cd40040d80153f52160aa28ea7e6115dde
K McNaught <kirsty.mcnaught@gmail.com>
2020-06-19 20:18:57 +0100

    Add missing file
    Should accompany cd7ceac06cfd184e5

---
+++ src/JuliusSweetland.OptiKey.EyeMine/UI/ValueConverters/StringOrEmptyLabel.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2020 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using JuliusSweetland.OptiKey.EyeMine.Properties;
+
+namespace JuliusSweetland.OptiKey.EyeMine.UI.ValueConverters
+{
+    // Return a string property, with a fallback value if empty
+    public class StringOrEmptyLabel : IValueConverter
+    {
+        public string EmptyLabel { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String fallback = String.IsNullOrEmpty(EmptyLabel) ? Resources.STRING_NOT_SET : EmptyLabel;
+
+            if (value == null || value == DependencyProperty.UnsetValue)
+                return fallback;
+
+            string stringVal = (string) value;
+            if (String.IsNullOrEmpty(stringVal))
+                return fallback;
+
+            return stringVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (String.Equals(value, EmptyLabel))
+                return "";
+            else
+                return value;
+        }
+    }
+}
