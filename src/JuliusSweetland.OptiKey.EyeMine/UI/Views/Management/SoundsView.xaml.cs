88c87761961e5bc397dc82a65244233a2415324e
K McNaught <kirsty.mcnaught@gmail.com>
2020-06-30 10:44:11 +0100

    Copy SoundsView for editing

---
+++ src/JuliusSweetland.OptiKey.EyeMine/UI/Views/Management/SoundsView.xaml.cs
@@ -0,0 +1,49 @@
+// Copyright (c) 2020 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+using JuliusSweetland.OptiKey.UI.ViewModels.Management;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JuliusSweetland.OptiKey.EyeMine.UI.Views.Management
+{
+    /// <summary>
+    /// Interaction logic for SoundsView.xaml
+    /// </summary>
+    public partial class SoundsView : UserControl
+    {
+        public SoundsView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindMaryTts_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                FileName = "marytts-server.bat",
+                Filter = "marytts-server|marytts-server.bat"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileLocation = null;
+
+                if (openFileDialog.FileName.EndsWith(@"\bin\marytts-server.bat"))
+                {
+                    txtMaryTtsLocation.Text = openFileDialog.FileName;
+                    fileLocation = txtMaryTtsLocation.Text;
+                }
+                else
+                {
+                    txtMaryTtsLocation.Text = Properties.Resources.MARYTTS_LOCATION_ERROR_LABEL;
+                }
+
+                SoundsViewModel viewModel = this.DataContext as SoundsViewModel;
+                if (viewModel != null && !string.IsNullOrWhiteSpace(fileLocation))
+                {
+                    viewModel.MaryTTSLocation = fileLocation;
+                }
+            }
+        }
+    }
+}
