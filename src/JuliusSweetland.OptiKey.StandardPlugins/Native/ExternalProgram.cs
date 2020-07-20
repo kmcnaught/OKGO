cec9a544ed1fa4b5ede4e8f453c562e6d8dd8ee6
K McNaught <kirsty.mcnaught@gmail.com>
2020-07-20 12:59:49 +0100

    WIPWIPWIP plugins!

--- src/JuliusSweetland.OptiKey.StandardPlugins/Native/ExternalProgram.cs
+++ src/JuliusSweetland.OptiKey.StandardPlugins/Native/ExternalProgram.cs
@@ -4,10 +4,13 @@ using System.Diagnostics;
 /**
  * This is a plugin that runs a command in windows. It can be anything that you would run using command line prompt.
  * 
- * It has only one method
+ * It has two methods
  * 
  * 1) RUN
- *    . command: the complete command lina that will be executed by the plugin
+ *    . command: the command that will be executed by the plugin
+ * 2) RUN_ARGS
+ *    . command: the command that will be executed by the plugin
+ *    . arguments: the arguments to pass
  * 
  * Please refer to OptiKey wiki for more information on registering and developing extensions.
  */
@@ -20,6 +23,12 @@ namespace JuliusSweetland.OptiKey.StandardPlugins
         public void RUN(string command)
         {
             Process.Start(command);
+        }
+
+        // Run with arguments
+        public void RUN_ARGS(string command, string arguments)
+        {
+            Process.Start(command, arguments);
         }
     }
 }
