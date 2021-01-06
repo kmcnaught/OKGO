e491ae1619f7913c98b975059f72881f67bb8024
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-01-06 23:39:00 +0000

    Add exe/installer signing

---
+++ sign_installer.bat
@@ -0,0 +1,2 @@
+
+installer\tools\signtool.exe sign /n "Open Source Developer, Kirsty McNaught" /t  http://time.certum.pl /fd sha256 /v  installer/Crayta/CraytaAccessSetup/Release/CraytaAccessSetup.msi
