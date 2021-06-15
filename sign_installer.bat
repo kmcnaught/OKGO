d8d3825e1766bf1a31a9fc664ac22796c6591b98
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-06-15 08:51:11 +0100

    Update signing script

--- sign_installer.bat
+++ sign_installer.bat
@@ -1,2 +1,2 @@
 
-installer\tools\signtool.exe sign /n "Open Source Developer, Kirsty McNaught" /t  http://time.certum.pl /fd sha256 /v  installer/Crayta/CraytaAccessSetup/Release/CraytaAccessSetup.msi
+installer\tools\signtool.exe sign /n "Open Source Developer, Kirsty McNaught" /t  http://time.certum.pl /fd sha256 /v  installer/Gaming/OKGameOnSetup/Release/OKGameOnSetup.msi
