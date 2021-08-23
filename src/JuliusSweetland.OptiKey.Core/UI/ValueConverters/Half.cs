93a9a64e3db4ec03640b24faaf83240cd8651a18
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-08-23 17:01:10 +0100

    Turn rectangle into ellipse (visual only)

---
+++ src/JuliusSweetland.OptiKey.Core/UI/ValueConverters/Half.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2020 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JuliusSweetland.OptiKey.UI.ValueConverters
+{
+    public class Half : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var num = (double)value;
+                return num / 2.0f;
+            }
+
+            return 0.0f;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
