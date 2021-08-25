572f47b451f04c4599656b1ae37adfdfe26d5c1c
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-08-25 21:21:53 +0100

    Update sensitivity calcs to use ellipse

--- src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Look2DInteractionHandler.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Look2DInteractionHandler.cs
@@ -287,33 +287,74 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
             return bounds;
         }
 
+        private Vector ellipseIntersection(Vector vector, double rx, double ry)
+        {
+            if (vector.LengthSquared == 0)
+            {
+                return new Vector(0, 0);
+            }
+            else {
+                // Intersection computed using parametric form for ellipse
+                // x = rx*cos(t), y = ry*sin(t) for parameter t [0,2π]
+                var t = Math.Atan2((rx * vector.Y), (ry * vector.X));
+
+                return new Vector(rx * Math.Cos(t), ry * Math.Sin(t));
+            }
+        }
+
         private Vector CalculateLookToScrollVelocity(Point current, Point centre)
         {
             double baseSpeed = 0;
             double acceleration = 0.02;
-            double hDeadzone = (double)Settings.Default.JoystickHorizontalDeadzonePercentScreen * Graphics.PrimaryScreenWidthInPixels / 100.0d;
-            double vDeadzone = hDeadzone / Settings.Default.JoystickDeadzoneAspectRatio;
 
-            var velocity = new Vector { X = 0, Y = 0 };
-            velocity.X = CalculateLookToScrollVelocity(
-                current.X,
-                centre.X,
-                0.5f * hDeadzone,
-                baseSpeed,
-                acceleration
-            );
+            
+            double deadzoneWidth = (double)Settings.Default.JoystickHorizontalDeadzonePercentScreen * Graphics.PrimaryScreenWidthInPixels / 100.0d;
+            double deadzoneHeight = deadzoneWidth / Settings.Default.JoystickDeadzoneAspectRatio;
 
-            velocity.Y = CalculateLookToScrollVelocity(
-                current.Y,
-                centre.Y,
-                0.5f * vDeadzone,
-                baseSpeed,
-                acceleration
+            Vector velocity = CalculateLookToScrollVelocityVec(
+                current, centre, 
+                deadzoneWidth, deadzoneHeight,
+                baseSpeed, acceleration
             );
 
             Log.DebugFormat("Current scrolling velocity is: {0}.", velocity);
 
             return velocity;
+        }
+
+        private double signedSqrt(double x)
+        {
+            // return sqrt of magnitude but with original sign
+            return Math.Sign(x) * Math.Sqrt(Math.Abs(x));
+        }
+
+        private Vector CalculateLookToScrollVelocityVec(
+            Point current,
+            Point centre,
+            double deadzoneWidth,
+            double deadzoneHeight,
+            double baseSpeed,
+            double acceleration)
+        {
+            // Calculate the direction and distance from the centre to the current value. 
+            Vector distance = current - centre;
+            Vector intersectionPoint = ellipseIntersection(distance, deadzoneWidth/2, deadzoneHeight/2);
+            if (distance.LengthSquared < intersectionPoint.LengthSquared)
+            {
+                // inside the deadzone
+                return new Vector(0, 0);
+            }
+            else
+            {
+                // Remove the deadzone.
+                distance -= intersectionPoint;
+
+                // Calculate total speed using base speed and distance-based acceleration.
+                Vector speed = new Vector(baseSpeed + signedSqrt(distance.X) * acceleration, 
+                                          baseSpeed + signedSqrt(distance.Y) * acceleration );         
+
+                return speed;
+            }
         }
 
         private double CalculateLookToScrollVelocity(
