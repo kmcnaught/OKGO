6b5a8dd79b47cf83f52b331d7ae2e5a771664b43
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-09-23 14:38:12 +0100

    Replace existing overlay XAML
    We need to make it more generic now

--- src/JuliusSweetland.OptiKey.Core/UI/Windows/LookToScrollOverlayWindow.xaml.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/Windows/LookToScrollOverlayWindow.xaml.cs
@@ -2,7 +2,11 @@
 using System;
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using JuliusSweetland.OptiKey.Static;
 using JuliusSweetland.OptiKey.UI.ViewModels;
 
 namespace JuliusSweetland.OptiKey.UI.Windows
@@ -13,6 +17,7 @@ namespace JuliusSweetland.OptiKey.UI.Windows
     public partial class LookToScrollOverlayWindow : Window
     {
         private readonly ILookToScrollOverlayViewModel viewModel;
+        Rectangle rect;
 
         public LookToScrollOverlayWindow(ILookToScrollOverlayViewModel viewModel)
         {
@@ -21,7 +26,28 @@ namespace JuliusSweetland.OptiKey.UI.Windows
             this.viewModel = viewModel;
             viewModel.PropertyChanged += ViewModel_PropertyChanged;
             DataContext = viewModel;
+            
+            this.AddEllipse(new Point(Graphics.PrimaryScreenWidthInPixels/2, Graphics.PrimaryScreenHeightInPixels/2), new Point(150, 150));
+
         }
+
+        private void AddEllipse(Point centre, Point radii)
+        {
+            rect = new Rectangle();
+            rect.Width = radii.X*2;
+            rect.Height = radii.Y*2;
+            rect.RadiusX = radii.X;
+            rect.RadiusY = radii.Y;            
+            rect.Stroke = Brushes.CadetBlue;
+            rect.Opacity = 0.25;
+            rect.StrokeThickness = 4;            
+
+            var top = centre - radii;
+            Canvas.SetTop(rect, top.Y);
+            Canvas.SetLeft(rect, top.X);
+
+            canvas.Children.Add(rect);
+        }        
 
         // Based on: https://stackoverflow.com/a/3367137/9091159
         protected override void OnSourceInitialized(EventArgs e)
@@ -36,16 +62,19 @@ namespace JuliusSweetland.OptiKey.UI.Windows
 
         private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            if (string.Equals("ActiveBounds", e.PropertyName))
+            if (string.Equals("ActiveDeadzone", e.PropertyName))
             {
-                Rect bounds = viewModel.ActiveBounds;
+                Rect bounds = viewModel.ActiveDeadzone;
                 if (!bounds.IsEmpty)
                 {
-                    Top = bounds.Top;
-                    Left = bounds.Left;
-                    Width = bounds.Width;
-                    Height = bounds.Height;
-                }
+                    Canvas.SetTop(rect, bounds.Top);
+                    Canvas.SetLeft(rect, bounds.Left);
+                    
+                    rect.Width = bounds.Width;
+                    rect.Height = bounds.Height;
+                    rect.RadiusX = bounds.Width / 2;
+                    rect.RadiusY = bounds.Height / 2;
+                }                
             }
         }
     }
