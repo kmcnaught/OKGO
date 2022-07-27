2e3602ad2ebf929ee12f9ce07fcc6661dc50e78a
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2022-07-27 17:26:49 +0100

    Fix loop key locking - but other issue

--- src/JuliusSweetland.OptiKey.Core/UI/ViewModels/MainViewModel.ServiceEventHandlers.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/MainViewModel.ServiceEventHandlers.cs
@@ -2727,11 +2727,14 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
                 //to one which toggles SimulateKeyStrokes, which results in all key states being stored and restored when leaving the keyboard. This
                 //incorrectly stores the key Down/LockedDown state, which is restored when we leave the keyboard. This *should* be safe as the command
                 //key's value is different from the key values of each individual command.
-                await CommandList(singleKeyValue, multiKeySelection, commandList, 0);
+                
+                // FIXME: I've reordered this to fix the locking of loop commands, but the above issue is presumably present
 
                 //if there is an override lock down time then do not set the key to LockedDown
                 keyStateService.KeyDownStates[singleKeyValue].Value = timeSpanOverrides != null && timeSpanOverrides.TimeRequiredToLockDown > TimeSpan.Zero
                     ? KeyDownStates.Down : KeyDownStates.LockedDown;
+
+                await CommandList(singleKeyValue, multiKeySelection, commandList, 0);
 
                 //if there is an override lock down time then run this key until the gaze stops or another trigger stops it
                 if (timeSpanOverrides != null && timeSpanOverrides.TimeRequiredToLockDown > TimeSpan.Zero)
