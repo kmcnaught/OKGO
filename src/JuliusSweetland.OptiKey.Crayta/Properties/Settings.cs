84bba2b9640cf715a51d53df7d23c789f9882db7
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2023-02-23 15:41:21 +0000

    Fix old minecraft reference
    this was causing error in management console

--- src/JuliusSweetland.OptiKey.Crayta/Properties/Settings.cs
+++ src/JuliusSweetland.OptiKey.Crayta/Properties/Settings.cs
@@ -169,7 +169,7 @@ namespace JuliusSweetland.OptiKey.Crayta.Properties {
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("EnglishMinecraft")]
+        [global::System.Configuration.DefaultSettingValueAttribute("EnglishUK")]
         [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
         public new global::JuliusSweetland.OptiKey.Enums.Languages KeyboardAndDictionaryLanguage
         {
